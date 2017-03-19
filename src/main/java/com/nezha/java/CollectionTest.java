dfbd2a0aee01da0df65effe9c7afe89def635f6d
nezha <nezhaxiaozi@outlook.com>
2017-03-19 21:03:42 +0800

    update

--- src/main/java/com/nezha/java/CollectionTest.java
+++ src/main/java/com/nezha/java/CollectionTest.java
@@ -1,5 +1,6 @@
 package com.nezha.java;
 
+import java.text.Collator;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,6 +12,7 @@ public class CollectionTest {
         List<Integer> list1 = new ArrayList<Integer>();
         System.out.println(list1.size());
         List<Integer> list2 = new LinkedList<Integer>();
+        Collections.sort(list1, Collator.getInstance(Locale.CHINA));
         Vector<Integer> vect1 = new Vector<Integer>();
         HashMap<String,Integer> hashMap1 = new HashMap<String, Integer>();
         Hashtable hashtable1 = new Hashtable();
@@ -21,9 +23,16 @@ public class CollectionTest {
         Collections.synchronizedMap(hashMap1);
         ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<String, String>();
         LinkedHashMap<String,Integer> hashMap2 = new LinkedHashMap<String, Integer>(16, 0.75f,true);
+        TreeMap<String,Integer> treeMap = new TreeMap<String, Integer>();
+    }
+}
 
+class compareTest implements Comparable,Comparator{
+    public int compareTo(Object o) {
+        return 0;
+    }
 
-
-
+    public int compare(Object o1, Object o2) {
+        return 0;
     }
 }
