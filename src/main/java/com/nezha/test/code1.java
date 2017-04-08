76ef392cd4e6caffe2400ce9f209ca7b38802e8e
nezha <nezhaxiaozi@outlook.com>
2017-04-08 16:31:27 +0800

    update

---
+++ src/main/java/com/nezha/test/code1.java
@@ -0,0 +1,39 @@
+package com.nezha.test;
+
+/**
+ * Created by 123 on 2017/4/7.
+ */
+public class code1 {
+        public static void main(String[] args) {
+            String s1 = "hello";
+            String s2 = "he"+new String("llo");
+            System.out.println(s1==s2);
+        }
+}
+
+class classA{
+    private final void Print(){
+        System.out.println("1");
+    }
+}
+
+class classB extends classA{
+    public classB(int a){
+
+    }
+
+
+    public static void main(String[] args){
+        
+    }
+}
+
+abstract class abstractTest{
+    public abstractTest(){
+
+    }
+    public void test(){
+
+    }
+}
+
