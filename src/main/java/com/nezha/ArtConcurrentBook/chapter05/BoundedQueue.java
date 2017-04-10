be3f4122c77e290a6749053ff95c5b348e51e121
nezha <nezhaxiaozi@outlook.com>
2017-04-10 19:53:54 +0800

    update

--- src/main/java/com/nezha/ArtConcurrentBook/chapter05/BoundedQueue.java
+++ src/main/java/com/nezha/ArtConcurrentBook/chapter05/BoundedQueue.java
@@ -3,6 +3,7 @@
  */
 package com.nezha.ArtConcurrentBook.chapter05;
 
+import java.util.Random;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -12,7 +13,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class BoundedQueue<T> {
     private Object[]  items;
-    // ��ӵ��±꣬ɾ�����±�����鵱ǰ����
+
     private int       addIndex, removeIndex, count;
     private Lock      lock     = new ReentrantLock();
     private Condition notEmpty = lock.newCondition();
@@ -22,12 +23,13 @@ public class BoundedQueue<T> {
         items = new Object[size];
     }
 
-    // ���һ��Ԫ�أ������������������߳̽���ȴ�״̬��ֱ���С���λ��
     public void add(T t) throws InterruptedException {
         lock.lock();
         try {
-            while (count == items.length)
+            while (count == items.length){
+                System.out.println("Queue full");
                 notFull.await();
+            }
             items[addIndex] = t;
             if (++addIndex == items.length)
                 addIndex = 0;
@@ -38,13 +40,16 @@ public class BoundedQueue<T> {
         }
     }
 
-    // ��ͷ��ɾ��һ��Ԫ�أ��������գ���ɾ���߳̽���ȴ�״̬��ֱ���������Ԫ��
+
     @SuppressWarnings("unchecked")
     public T remove() throws InterruptedException {
         lock.lock();
         try {
-            while (count == 0)
+            while (count == 0){
+                System.out.println("Queue empty");
                 notEmpty.await();
+            }
+
             Object x = items[removeIndex];
             if (++removeIndex == items.length)
                 removeIndex = 0;
@@ -53,6 +58,89 @@ public class BoundedQueue<T> {
             return (T) x;
         } finally {
             lock.unlock();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ""+items[0]+"-"+items[1];
+    }
+}
+
+class ThreadTest{
+    public static BoundedQueue<Integer> boundedQueue = new BoundedQueue<Integer>(2);
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new putRunnable(boundedQueue),"Thread1 ");
+        Thread t2 = new Thread(new popRunnable(boundedQueue),"Thread2 ");
+        Thread t3 = new Thread(new putOneRunnable(boundedQueue),"Thread3 ");
+        try {
+            t1.start();
+            Thread.sleep(2000);
+            t2.start();
+            Thread.sleep(2000);
+            t3.start();
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+class popRunnable implements Runnable{
+    BoundedQueue<Integer> boundedQueue;
+    public  popRunnable(BoundedQueue boundedQueue){
+        this.boundedQueue = boundedQueue;
+    }
+    public void run() {
+        try {
+            boundedQueue.remove();
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+            boundedQueue.remove();
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+            boundedQueue.remove();
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+            boundedQueue.remove();
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class putOneRunnable implements Runnable{
+    BoundedQueue<Integer> boundedQueue;
+    public  putOneRunnable(BoundedQueue boundedQueue){
+        this.boundedQueue = boundedQueue;
+    }
+    public void run() {
+        try {
+            boundedQueue.add(4);
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class putRunnable implements Runnable{
+    BoundedQueue<Integer> boundedQueue;
+    public  putRunnable(BoundedQueue boundedQueue){
+        this.boundedQueue = boundedQueue;
+    }
+    public void run(){
+        try {
+            boundedQueue.add(1);
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+            boundedQueue.add(2);
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+            boundedQueue.add(3);
+            System.out.println(Thread.currentThread().getName()+boundedQueue.toString());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/nezha/test/test.java
+++ src/main/java/com/nezha/test/test.java
@@ -7,50 +7,17 @@ import java.util.StringTokenizer;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by 123 on 2017/3/14.
  */
 public class test {
-    public final int COUNT = 10;
-    public static void  main(String[] args){
-
-    }
-
-    public int getJudge(){
-        try {
-            System.out.println(COUNT);
-            return 0;
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            return 1;
-        }
+    public static void main(String[] args) {
+        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(20));
     }
 
 }
 
-
-abstract class shape{
-    public abstract void getArea();
-}
-
-
-class shapeA extends shape{
-    public static String a = "Hello";
-    public shapeA() {
-        System.out.println("shapeA init "+super.getClass().getName());
-    }
-
-    @Override
-    public void getArea() {
-        System.out.println("shapeA");
-    }
-
-    class innerClass{
-        public innerClass(){
-            System.out.println(super.getClass().getName());
-        }
-    }
-}
 
